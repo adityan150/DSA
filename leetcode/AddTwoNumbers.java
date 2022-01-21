c1418f42d1a6d78434a030a165c266b64c3d1689
adityan150 <adityanautiyal150@gmail.com>
2022-01-21 13:48:13 +0530

    Minor changes in code: AddTwoNumbers; Placed ListNode in seperate file; Leetcode/SwapNodesInPairs

--- leetcode/AddTwoNumbers.java
+++ leetcode/AddTwoNumbers.java
@@ -1,13 +1,13 @@
 package com.aditya.DSA.leetcode;
 import java.util.Scanner;
 // Definition for singly-linked list.
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
+//class ListNode {
+//    int val;
+//    ListNode next;
+//    ListNode() {}
+//    ListNode(int val) { this.val = val; }
+//    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+//}
 
 public class AddTwoNumbers {
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
